90cbeb7f8d575eaf9315689285df7f5b25562639
a172862967 <a172862967@gmail.com>
2018-05-29 18:47:35 +0800

    Upgrade Demo

--- Assets/VolumeVertex/Script/VolumeVertex.cs
+++ Assets/VolumeVertex/Script/VolumeVertex.cs
@@ -1,23 +1,43 @@
 ﻿using UnityEngine;
 using System.Linq;
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct targetData
+{
+    [SerializeField]
+    GameObject m_targetObj;
+    public GameObject targetObj
+    {
+        get { return m_targetObj; }
+        set { m_targetObj = value; }
+    }
+
+    [SerializeField, ColorUsage(false, true)]
+    Color m_emissionColor;
+    public Color emissionColor
+    {
+        get { return m_emissionColor; }
+        set { m_emissionColor = value; }
+    }
+
+    [SerializeField]
+    float m_powItensity;
+    public float powItensity
+    {
+        get { return m_powItensity; }
+        set { m_powItensity = value; }
+    }
+}
+
 
 [ExecuteInEditMode, AddComponentMenu("KG/VolumeVertex")]
 public class VolumeVertex : MonoBehaviour {
 
-    [System.Serializable]
-    public struct targetData
+    [SerializeField]
+    List<targetData> m_target = new List<targetData>();
+	public List<targetData> Target
     {
-        public GameObject m_targetObj;
-
-        [ColorUsage(false, true)]
-        public Color m_emissionColor;
-
-        public float m_powItensity;
-    }
-
-	[SerializeField]
-    targetData[] m_target;
-	public targetData[] Target{
 		get{return m_target; }
 		set{ m_target = value;}
 	}
@@ -51,16 +71,16 @@ public class VolumeVertex : MonoBehaviour {
         }
 
          var _pos = from _s in m_target
-                    select new Vector4(_s.m_targetObj.transform.position.x, 
-                    _s.m_targetObj.transform.position.y, 
-                    _s.m_targetObj.transform.position.z,
+                    select new Vector4(_s.targetObj.transform.position.x, 
+                    _s.targetObj.transform.position.y, 
+                    _s.targetObj.transform.position.z,
                     1);
 
         var _color = from _s in m_target
-                   select _s.m_emissionColor;
+                   select _s.emissionColor;
 
         var _pow = from _s in m_target
-                     select _s.m_powItensity;
+                     select _s.powItensity;
 
         m_mat.SetVectorArray("_TargetPos", _pos.ToList());
         m_mat.SetColorArray("_TargetColor", _color.ToList());
